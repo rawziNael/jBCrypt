1f1832033859653f231863ef399e240320fbed74
rawziNael <rawzi.homran@yahoo.com>
2022-04-04 23:23:08 +0300

    lab14

---
+++ jBCrypt/src/test/java/com/example/jBCrypt/JBCryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jBCrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JBCryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
